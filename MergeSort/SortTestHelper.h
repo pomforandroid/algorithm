442069c5987181f8883f3607fde16646a57780ed
wupengcong <634273025@qq.com>
2021-07-08 22:24:21 +0800

    add mergesort detail

--- MergeSort/SortTestHelper.h
+++ MergeSort/SortTestHelper.h
@@ -63,6 +63,30 @@ namespace SortTestHelper{
         return arr;
     }
 
+
+    template<typename T>
+    void insertionSort(T arr[], int n){
+        for(int i=1; i <n; i++){
+            T e = arr[i];
+            int j ;//save the position element e
+            for(j=i; j>0 && arr[j-1]>e; j--){
+                arr[j] = arr[j-1];
+            }
+            arr[j] = e;
+        }
+    }
+
+    template<typename T>
+    void insertionSort(T arr[], int l, int r){
+        for(int i=l; i <r; i++){
+            T e = arr[i];
+            int j ;//save the position element e
+            for(j=i; j>0 && arr[j-1]>e; j--){
+                arr[j] = arr[j-1];
+            }
+            arr[j] = e;
+        }
+    }
 }
 
 #endif //SELECTIONSORT_SORTTESTHELPER_H
--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -1,6 +1,71 @@
 #include <iostream>
+#include <algorithm>
+#include "SortTestHelper.h"
+using namespace std;
+
+template<typename T>
+void __merge(T arr[], int l, int mid, int r){
+    T aux[r-l+1];
+    for(int i=l; i<=r; i++){
+        aux[i-l] = arr[i];
+    }
+    int i = l;
+    int j = mid + 1;
+    for(int k=l; k<=r; k++){
+        if(i>mid){
+            arr[k] = aux[j-l];
+            j++;
+        }else if(j>r){
+            arr[k] = aux[i-l];
+            i++;
+        }else if(aux[i-l] < aux[j-l]){
+            arr[k] = aux[i-l];
+            i++;
+        }else{
+            arr[k] = aux[j-l];
+            j++;
+        }
+    }
+
+}
+
+
+//递归使用递归排序，对arr[l,r]的范围进行排序
+template<typename T>
+void __mergesort(T arr[], int l, int r){
+    if(r-l > 1024){
+        SortTestHelper::insertionSort(arr, l, r);
+        return;
+    }
+
+    int mid = (l + r) /2;
+
+    __mergesort(arr, l, mid);
+    __mergesort(arr, mid+1, r);
+    if(arr[mid] > arr[mid+1])
+        __merge(arr, l, mid, r);
+
+}
+
+template<typename T>
+void mergeSortBU(T arr[], int n){
+    for(int sz=1; sz < n; sz+=sz)
+        for(int i=0; i+sz < n; i+= sz+sz )
+            __merge(arr, i, i+sz-1, min(i+sz+sz-1, n-1));
+}
+
+template<typename T>
+void mergeSort(T arr[], int n){
+    __mergesort(arr ,0, n-1);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n = 50000;
+    int* arr = SortTestHelper::generateRandomNearArray(n,10);
+    int* arr2 =SortTestHelper::copyIntArray(arr, n);
+    SortTestHelper::testSort("MergeBU", mergeSortBU, arr2, n);
+    SortTestHelper::testSort("merge", mergeSort, arr, n);
+
+
     return 0;
 }
