8dc0ce79b850816f48034bf7b2fa41ae1faa0881
fable.wu <fable.wu@example.com>
2021-08-13 10:55:17 +0800

    Heap sort Optimized!

--- MaxHeap/MaxHeap.cpp
+++ MaxHeap/MaxHeap.cpp
@@ -9,6 +9,21 @@
 #include "QuickSort2Ways.h"
 #include "QuickSort3Ways.h"
 #include "SortTestHelper.h"
+template<typename T>
+void shiftDown(T arr[], int n, int k) {
+    while (k * 2 + 1 < n) // k*2 + 1 is k's left child ,if k * 2 + 1 > count ,it means that k have no left child
+    {
+        int j = k * 2 + 1;
+        if (j + 1 < n && arr[j + 1] > arr[j])
+            j += 1;
+
+        if (arr[k] > arr[j])
+            break;
+
+        swap(arr[k], arr[j]);
+        k = j; // update the k 
+    }
+}
 
 
 template<typename T>
@@ -33,6 +48,21 @@ void heapSort2(T arr[], int n) {
     }
 }
 
+
+template<typename T>
+void heapSort(T arr[], int n) {
+    for (int i = (n - 2)/ 2; i >=0; i--)
+        shiftDown(arr, n, i); //heapify 
+
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[i], arr[0]);
+        shiftDown(arr, i, 0);
+    }
+
+}
+
+
+
 // 比较 Merge Sort, 三种 Quick Sort 和本节介绍的两种 Heap Sort 的性能效率
 // 注意, 这几种排序算法都是 O(nlogn) 级别的排序算法
 int main() {
@@ -47,6 +77,7 @@ int main() {
     int* arr4 = SortTestHelper::copyIntArray(arr1, n);
     int* arr5 = SortTestHelper::copyIntArray(arr1, n);
     int* arr6 = SortTestHelper::copyIntArray(arr1, n);
+    int* arr7 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
@@ -54,6 +85,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
     SortTestHelper::testSort("Heap Sort 1", heapSort1, arr5, n);
     SortTestHelper::testSort("Heap Sort 2", heapSort2, arr6, n);
+    SortTestHelper::testSort("Heap Sort ", heapSort, arr7, n);
 
     delete[] arr1;
     delete[] arr2;
@@ -61,6 +93,7 @@ int main() {
     delete[] arr4;
     delete[] arr5;
     delete[] arr6;
+    delete[] arr7;
 
     cout << endl;
 
@@ -74,6 +107,7 @@ int main() {
     arr4 = SortTestHelper::copyIntArray(arr1, n);
     arr5 = SortTestHelper::copyIntArray(arr1, n);
     arr6 = SortTestHelper::copyIntArray(arr1, n);
+    arr7 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
@@ -81,6 +115,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
     SortTestHelper::testSort("Heap Sort 1", heapSort1, arr5, n);
     SortTestHelper::testSort("Heap Sort 2", heapSort2, arr6, n);
+    SortTestHelper::testSort("Heap Sort ", heapSort, arr7, n);
 
     delete[] arr1;
     delete[] arr2;
@@ -88,6 +123,7 @@ int main() {
     delete[] arr4;
     delete[] arr5;
     delete[] arr6;
+    delete[] arr7;
 
     cout << endl;
 
@@ -100,6 +136,7 @@ int main() {
     arr4 = SortTestHelper::copyIntArray(arr1, n);
     arr5 = SortTestHelper::copyIntArray(arr1, n);
     arr6 = SortTestHelper::copyIntArray(arr1, n);
+    arr7 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     // 这种情况下, 普通的QuickSort退化为O(n^2)的算法, 不做测试
@@ -108,6 +145,7 @@ int main() {
     SortTestHelper::testSort("Quick Sort 3 Ways", quickSort3Ways, arr4, n);
     SortTestHelper::testSort("Heap Sort 1", heapSort1, arr5, n);
     SortTestHelper::testSort("Heap Sort 2", heapSort2, arr6, n);
+    SortTestHelper::testSort("Heap Sort ", heapSort, arr7, n);
 
     delete[] arr1;
     delete[] arr2;
@@ -115,6 +153,7 @@ int main() {
     delete[] arr4;
     delete[] arr5;
     delete[] arr6;
+    delete[] arr7;
 
     return 0;
 }
