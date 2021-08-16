858afb457376f7b577be693fb897a411b5b87ed3
fable.wu <fable.wu@example.com>
2021-08-16 16:46:00 +0800

    add BinarySearchTree base data constructor

---
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -0,0 +1,55 @@
+﻿// BinarySearchTree.cpp : Defines the entry point for the application.
+//
+
+#include "BinarySearchTree.h"
+
+using namespace std;
+
+template <typename Key, typename Value>
+class BST {
+
+private:
+	struct Node
+	{
+		Key key;
+		Value value;
+		Node* left;
+		Node* right;
+		// struct 's constructor included in struct body
+		Node(Key key, Value value) {
+			this->key = key;
+			this->value = value;
+			this->left = this->value = null;
+		}
+
+	};
+	
+	Node* root;
+	int count;
+	
+public:
+	
+	BST() {
+		root = NULL;
+		count = 0;
+	}
+
+	~BST() {
+		// TODO 
+	}
+
+	int size() {
+		return count;
+	}
+
+	bool isEmpty() {
+		return count == 0;
+	}
+
+};
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
