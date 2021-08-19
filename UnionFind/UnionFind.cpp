09e37344b071ef602d9e18630492b1bcc89720e4
fable.wu <fable.wu@example.com>
2021-08-19 16:53:41 +0800

    add UnionFind Quick basic

---
+++ UnionFind/UnionFind.cpp
@@ -0,0 +1,20 @@
+﻿// UnionFind.cpp : Defines the entry point for the application.
+//
+
+#include "UnionFind.h"
+#include <iostream>
+#include "UnionFindTestHelper.h"
+
+using namespace std;
+
+int main()
+{
+    // 使用10000的数据规模
+    int n = 100000;
+
+    // 虽然isConnected只需要O(1)的时间, 但由于union操作需要O(n)的时间
+    // 总体测试过程的算法复杂度是O(n^2)的
+    UnionFindTestHelper::testUF1(n);
+
+    return 0;
+}
---
+++ UnionFind/UnionFind.h
@@ -0,0 +1,54 @@
+﻿// UnionFind.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+#include <cassert>
+
+using namespace std;
+
+namespace UF1 {
+	class UnionFind {
+
+	private:
+		int count;
+		int* id;
+
+	public:
+		UnionFind(int n) {
+			this->count = n;
+			id = new int[n];
+			for (int i = 0; i < n;i++)
+				id[i] = i;
+		}
+
+		~UnionFind() {
+			delete id;
+		}
+
+		int find(int p) {
+			assert(p >= 0 && p < this->count);
+			return this->id[p];
+		}
+
+		bool isConnected(int p, int q) {
+			return find(p) == find(q);
+		}
+
+		void unionElements(int p, int q) {
+			int pID = find(p);
+			int qID = find(q);
+
+			for (int i = 0; i < count;i++) {
+				if (id[i] == pID) {
+					id[i] = qID;
+				}
+			}
+		}
+	};
+}
+
